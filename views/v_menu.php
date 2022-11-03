2f6cb9f2bc181a5780df75ef10c1adba546869a2
AGBANGLANON Stéphanie <stephanie.agbanglanon@etu.univ-lyon1.fr>
2022-11-03 06:46:50 +0100

    réalisation du menu

--- views/v_menu.php
+++ views/v_menu.php
@@ -1,5 +1,13 @@
-<?php
+<nav>
+    <div class="connection">
+        Connexion
+    </div>  
+    <div class="menu">
+        <ul>
+            <li>Accueil</li>
+            <li>Billetterie</li>
+            <li>Tournois</li>
+        </ul>
+    </div>
+</nav>
 
-echo("hi");
-
-?>
