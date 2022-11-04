c4188885e4698fe58fd5d35b233c5f08ae3e6368
AGBANGLANON Stéphanie <stephanie.agbanglanon@etu.univ-lyon1.fr>
2022-11-04 23:22:55 +0100

    modification de la vue d'achat des billets : ajout des infos de l'acheteur

--- Billetterie/controllers/c_achat.php
+++ Billetterie/controllers/c_achat.php
@@ -1,5 +1,16 @@
 <?php
 
+if(isset($_POST['choixmatch']) 
+    && isset($_POST['choixdate']) 
+    && isset($_POST['hours']) 
+    && isset($_POST['choixqte']) 
+    && isset($_POST['choixplace'])){
+    
+
+    
+    
+}
 
 require_once(PATH_VIEWS.'achat.php');
+
 ?>
--- views/v_achat.php
+++ views/v_achat.php
@@ -3,53 +3,78 @@ require_once(PATH_CONTROLLERS_P.'header.php');
 ?>
 
 <form class="achat" action="" method="post">
-    <div class="match">
-        <label for="choixmatch">Match :</label>
-        <select name="choixmatch" id="choixmatch">
-            <option value="">1/16 de finale</option>
-            <option value="">1/8 de finale</option>
-            <option value="">1/4 de finale</option>
-            <option value="">1/2 finale</option>
-            <option value="">finales</option>
-        </select>
-    </div>
-   
-    <div class="date">
-        <label for="choixdate">Date :</label>
-        <input type="date" name="choixdate" id="choixdate">
-    </div>
-   
 
-    <div class="heure">
-        <label for="hours">Heure :</label>
-        <select name="hours" id="hours">
-            <option value="">10h</option>
-            <option value="">12h</option>
-            <option value="">14h</option>
-            <option value="">16h</option>
-        </select>
-    </div>
+    <fieldset>
+        <legend>Match</legend>
+        <div class="match">
+            <label for="choixmatch">Match :</label>
+            <select name="choixmatch" id="choixmatch">
+                <option value="">1/16 de finale</option>
+                <option value="">1/8 de finale</option>
+                <option value="">1/4 de finale</option>
+                <option value="">1/2 finale</option>
+                <option value="">finales</option>
+            </select>
+        </div>
+    
+        <div class="date">
+            <label for="choixdate">Date :</label>
+            <input type="date" name="choixdate" id="choixdate">
+        </div>
     
 
-    <div class="qté">
-        <label for="choixqte">Quantité :</label>
-        <input type="number" name="choixqte" id="choixqte">
-    </div>
-    
+        <div class="heure">
+            <label for="hours">Heure :</label>
+            <select name="hours" id="hours">
+                <option value="">10h</option>
+                <option value="">12h</option>
+                <option value="">14h</option>
+                <option value="">16h</option>
+            </select>
+        </div>
+        
 
-    <div class="place">
-        <label for="choixplace">Choisir une place :</label>
-        <select name="choixplace" id="choixplace">
-            <option value="">Loge</option>
-            <option value="">Catégorie 1</option>
-            <option value="">Catégorie 2</option>
-        </select>
-    </div>
-    
+        <div class="qté">
+            <label for="choixqte">Quantité :</label>
+            <input type="number" name="choixqte" id="choixqte">
+        </div>
+        
+
+        <div class="place">
+            <label for="choixplace">Choisir une place :</label>
+            <select name="choixplace" id="choixplace">
+                <option value="">Loge</option>
+                <option value="">Catégorie 1</option>
+                <option value="">Catégorie 2</option>
+            </select>
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Identité</legend>
+        <div>
+            <label for="nom">Nom :</label>
+            <input type="text" name="nom" id="nom">
+        </div>
+
+        <div>
+            <label for="prenom">Prénom :</label>
+            <input type="text" name="prenom" id="prenom">
+        </div>
+
+        <div>
+            <label for="email">Adresse mail :</label>
+            <input type="text" name="email" id="email">
+        </div>
+        
+    </fieldset>
+
     <div class="button">
         <button type="reset" id="annuler">Annuler</button>
         <button type="submit" id="ok">Acheter</button>
     </div>
+
+    
    
 </form>
 
